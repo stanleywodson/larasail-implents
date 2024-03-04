216e2e25e2d72065aef2402d760ab7b8e47f93b1
stanleywodson <stanleypt@outlook.com>
2024-03-04 17:47:55 -0300

    start project

---
+++ app/Console/Commands/Report.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Report extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'report {startdate} {enddate}';
+
+    protected $description = 'Generate report of all users created between two dates';
+
+    public function handle()
+    {
+        //
+    }
+}
